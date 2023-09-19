7c3933e7ef2e91759a24d7dd33b7120d1d6db9fa
andersonbosa <andersonbosa0@gmail.com>
2023-09-19 08:47:12 -0300

    add new test case

--- test/EnterParkingLot.test.ts
+++ test/EnterParkingLot.test.ts
@@ -1,9 +1,9 @@
 import EnterParkingLot from "../src/core/usecase/EnterParkingLot"
 import GetParkingLot from "../src/core/usecase/GetParkingLot"
-import { ParkingLotIsFullError } from '../src/errors'
+import { ParkingLotIsCloseError, ParkingLotIsFullError } from '../src/errors'
 import ParkingLotRepositoryMemory, { ParkingLotCodes } from "../src/infra/repository/ParkingLotRepositoryMemory"
 
-const getEnterDate = () => new Date("2023-09-19 12:00:00")
+const getEnterDate = (): Date => new Date("2023-09-19 12:00:00")
 
 test("Should get parking lot", async function () {
 	const parkingLotRepositoryMemory = new ParkingLotRepositoryMemory()
@@ -30,8 +30,6 @@ test("Should enter parking lot", async () => {
 	expect(parkingLotAfterEnter.occupiedSpaces).toBe(1)
 })
 
-
-
 test("Should be closed", async function () {
 	const parkingLotRepositoryMemory = new ParkingLotRepositoryMemory()
 
@@ -39,8 +37,35 @@ test("Should be closed", async function () {
 	const parkingLotBeforeEnter = await getParkingLot.execute(ParkingLotCodes.shopping)
 	expect(parkingLotBeforeEnter.occupiedSpaces).toBe(0)
 
-	const enterParkingLot = new EnterParkingLot(parkingLotRepositoryMemory)
-	await enterParkingLot.execute(ParkingLotCodes.shopping, "MMM-0001", getEnterDate())
+	try {
+		const enterParkingLot = new EnterParkingLot(parkingLotRepositoryMemory)
+		const closedHour = new Date(new Date().setHours(23))
+		await enterParkingLot.execute(ParkingLotCodes.shopping, "MMM-0001", closedHour)
+	} catch (error) {
+		expect(error instanceof ParkingLotIsCloseError).toBe(true)
+		expect(error.message).toBe("The parking lot is closed")
+	}
 })
-  expect(true)
+
+test("Should be full", async () => {
+	const parkingLotRepositoryMemory = new ParkingLotRepositoryMemory()
+
+	const getParkingLot = new GetParkingLot(parkingLotRepositoryMemory)
+	const parkingLotBeforeEnter = await getParkingLot.execute(ParkingLotCodes.shopping)
+	expect(parkingLotBeforeEnter.occupiedSpaces).toBe(0)
+
+
+	const enterParkingLot = new EnterParkingLot(parkingLotRepositoryMemory)
+
+	// Enter vehicles to fill the parking lot capacity
+	for (let i = 1; i <= parkingLotBeforeEnter.capacity; i++) {
+		enterParkingLot.execute(ParkingLotCodes.shopping, `MMM-000${i}`, getEnterDate())
+	}
+
+	try {
+		enterParkingLot.execute(ParkingLotCodes.shopping, "MMM-0006", getEnterDate())
+	} catch (error) {
+		expect(error instanceof ParkingLotIsFullError).toBe(true)
+		expect(error.message).toBe("The parking lot is full")
+	}
 })
